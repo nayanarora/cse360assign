64998dc09268347ee93ae9b7434ad2e5dd27cb9a
System Administrator <root@Nayans-MacBook-Pro.local>
2019-02-25 21:20:19 -0700

    Part A

---
+++ cse360assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,78 @@
+/**------------------------------------------------------------------------------
+// AUTHOR            : Nayan Arora
+// CLASS ID          : 319  
+// ASSIGNMENT NUMBER : Assignment 2
+// DESCRIPTION       : Implement a class calculator.java and implementation on github.
+//--------------------------------------------------------------------------------*/
+
+package cse360assign2;
+
+/**
+ * @author nayanarora
+ * The public class calculator has 7 methods to implement the working of a normal calculator. 
+ * It can add, subtract, multiply or divide. It can also show history and calculate the total. 
+ * It has one private member that is the integer variable total.
+ */
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * The getTotal method is used for calculating the total after the implementation of the calculations.
+	 * @return
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/**
+	 * The add method is used for adding the value parameter to the total to get the sum.
+	 * @param value is used for getting user input and adding it to the total.
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**
+	 * The subtract method is used for subtracting the value parameter from the total to get the result.
+	 * @param value is used for getting user input and subtracting it from total to get the final result.
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/**
+	 * The multiply method is used for multiplying the value parameter with the total to get the result.
+	 * @param value is used for getting the user input and multiplying that with total to get result.
+	 */
+	public void multiply (int value)
+	{
+		
+	}
+	
+	/**
+	 * The divide method is used for dividing the value with the value in total to get the result.
+	 * @param value is used for getting user input and dividing that with the value in total to get the result.
+	 */
+	public void divide (int value) 
+	{
+		
+	}
+	
+	/**
+	 * The getHistory method is used for returning the history of all actions as a string. 
+	 * @return
+	 */
+	public String getHistory () 
+	{
+		return "";
+	}
+}
